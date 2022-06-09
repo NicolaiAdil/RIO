fe2f2a4f85957edb4035edb26a0f8b5e757c0ee5
Jakob Jernsletten <jakobj@stud.ntnu.no>
2022-06-09 16:58:12 +0200

    #3515: Removed comments

--- camera-lidar/lidar_processing/src/lidar_clustering.cpp
+++ camera-lidar/lidar_processing/src/lidar_clustering.cpp
@@ -8,10 +8,6 @@
 #include "pcl_ros/transforms.h"
 #include "sensor_msgs/PointCloud2.h"
 #include <pcl_conversions/pcl_conversions.h>
-//#include "pcl/surface/convex_hull.h"
-//#include "tf2/convert.h"
-//#include "tf2_geometry_msgs/tf2_geometry_msgs.h"
-//#include "tf2_ros/transform_listener.h"
 #include "tf/transform_listener.h"
 #include <cmath>
 
@@ -33,24 +29,18 @@ pcl::PointCloud<pcl::PointXYZ> *land_cloud = NULL;
 
 // Publisher for autosea radarscan/lidarscan
 ros::Publisher scanpub;
-// tf2_ros::Buffer tfBuffer;
 
 double filtered_points = 0.0;
 double total_points = 0.0;
 double iterator = 0.0;
 double avg_centroid = 0.0;
 
-// void velodyne_points_cb(const sensor_msgs::PointCloud2ConstPtr& input) {
 void velodyne_points_cb(
     const pcl::PointCloud<pcl::PointXYZI>::ConstPtr &input) {
 
   // Change Timestamp to gps-time during normal run
   ros::Time pc2Stamp = ros::Time::now();
-  // ros::Time pc2Stamp = input->header.stamp;
-  //    ROS_INFO_STREAM("pc2 received " << pc2Stamp.toSec());
   geometry_msgs::TransformStamped transformStamped;
-  //    tf2::Stamped<tf2::Transform> tf2_transform;
-  //    tf2::Transform tf2_transform;
   tf::StampedTransform transform;
   tf::StampedTransform transform2;
   tf::StampedTransform fixed_ned_transform;
@@ -67,8 +57,6 @@ void velodyne_points_cb(
   double pc2Stamp2 = ros::Time::now().toSec();
   double tnow;
 
-  //(ROS_INFO("Time-difference between gpstime and ros-time in sec %f",);
-
   try {
     pListener->waitForTransform("fixed", "lidar", pc2Stamp, ros::Duration(1.0));
     tnow = ros::Time::now().toSec();
@@ -78,14 +66,12 @@ void velodyne_points_cb(
     pListener->lookupTransform("body", "body", pc2Stamp, test_transform);
 
     double tlookup = ros::Time::now().toSec();
-    //        tf2::fromMsg(transformStamped.transform, tf2_transform);
     pcl_ros::transformPointCloud(*input, fixed_cloud, transform);
     fixed_cloud.header.frame_id = "fixed";
     fixed_cloud.header.stamp = input->header.stamp;
     fixed_cloud.header.seq = input->header.seq;
 
     double ttrans = ros::Time::now().toSec();
-    // ROS_INFO_STREAM("Header: " << input->header);
 
     pcl::PointCloud<pcl::PointXYZ> lidar_pt;
     lidar_pt.header.frame_id = "ned";
@@ -97,7 +83,6 @@ void velodyne_points_cb(
     pcl_ros::transformPointCloud(lidar_pt, lidar_ned_pt, transform2);
     float lidar_n = lidar_ned_pt.points[0].x;
     float lidar_e = lidar_ned_pt.points[0].y;
-    // ROS_INFO_STREAM("Lidar ne: " << lidar_n << " " << lidar_e);
 
     pcl_ros::transformPointCloud(*input, ned_cloud, transform2);
     ned_cloud.header.frame_id = "ned";
@@ -120,27 +105,13 @@ void velodyne_points_cb(
     }
     double fstop = ros::Time::now().toSec();
     double nstart = ros::Time::now().toSec();
-    /*        typedef
-       nanoflann::KDTreeSingleIndexAdaptor<nanoflann::L2_Simple_Adaptor<float,
-       PtCloud>, PtCloud, 2> my_kd_tree_t; my_kd_tree_t index(2, kd_cloud,
-       nanoflann::KDTreeSingleIndexAdaptorParams()); index.buildIndex();*/
     pcl::PointCloud<pcl::PointXYZ> centroids = DBScan::cluster(kd_cloud);
     double nstop = ros::Time::now().toSec();
-    // ROS_INFO_STREAM("Cluster time: " << nstop-nstart << " filter time: " <<
-    // fstop-fstart << "Total time: " << (nstop-nstart)+(fstop-fstart) << "
-    // points: " << counter);
     pub.publish(fixed_cloud);
-    // ROS_INFO_STREAM("nanoflann build time: " << nstop-nstart << " filter
-    // time: " << fstop-fstart << " points: " << counter << " Total size: " <<
-    // ned_cloud.size());
     filtered_points += counter;
     total_points += ned_cloud.size();
     avg_centroid += centroids.size();
     iterator += 1;
-
-    // ROS_INFO_STREAM("avg unfiltered: " << total_points/iterator << " avg
-    // amount after filtering : " << filtered_points/iterator << " Avg amount of
-    // centroids : " << avg_centroid/iterator);
 
     // Message for PDAF-tracker(autosea)
     autosea_msgs::RadarScan scanMsg;
@@ -150,7 +121,6 @@ void velodyne_points_cb(
       msg.header.frame_id = "ned";
       pcl_conversions::fromPCL(input->header.stamp, rosTimeStamp);
       msg.header.stamp = rosTimeStamp;
-      // ROS_INFO_STREAM("Time: " << rosTimeStamp);
       msg.type = 0; // point
 
       msg.centroid.x = centroids.points[i].x;
@@ -172,7 +142,6 @@ void velodyne_points_cb(
     ros::Time endtime = ros::Time::now();
 
     double total_clustering_time = endtime.toSec() - pc2Stamp.toSec();
-    // ROS_INFO_STREAM("Total cluster time: " << total_clustering_time);
 
     pcl::PointCloud<pcl::PointXYZI> filtered_cloud;
     filtered_cloud.header.frame_id = "fixed";
@@ -186,81 +155,12 @@ void velodyne_points_cb(
     pcl_ros::transformPointCloud(filtered_cloud, fixed_filtered_cloud,
                                  fixed_ned_transform);
     pubfiltered.publish(fixed_filtered_cloud);
-    // ROS_INFO_STREAM("pc2 lookup time: " << tlookup-tnow << " total time: " <<
-    // ttrans-tnow);
-    // Test rotations
-    // pcl::PointCloud<pcl::PointXYZI> init_cloud;
-    // init_cloud.header.frame_id = "fixed";
-    // init_cloud.height = 1;
-    // init_cloud.width = 1;
-    //        init_cloud.points.push_back(pcl::PointXYZI(10.0, 0.0, 0.0));
-    // init_cloud.points.push_back(pcl::PointXYZI(0.0));
-    //        init_cloud.points[0].y = 0.0;
-    //        pubinit.publish(init_cloud);
-    //        const pcl::PointCloud<pcl::PointXYZ> init_cloud2 = init_cloud;
-    //        tf::Transform identity;
-    //        tf::Transform test = transform * identity.getIdentity();
-
-    // pcl_ros::transformPointCloud(init_cloud, trans_cloud, transform);
-    // trans_cloud.points[0].intensity = 30.0;
-    // pubtrans.publish(trans_cloud);
-    /*        pcl_ros::transformPointCloud(init_cloud, trans_cloud2,
-       transform2); trans_cloud2.points[0].intensity = 20.0;
-            pubtrans2.publish(trans_cloud2);
-    */
-    // pcl::PointCloud<pcl::PointXYZ> test_cloud;
-    // test_cloud.header.frame_id = "body";
-    // test_cloud.height = 1;
-    // test_cloud.width = 5;
-    // test_cloud.points.emplace_back(pcl::PointXYZ(5, 0.0, 0.0));
-    // test_cloud.points.emplace_back(pcl::PointXYZ(15.0, 0.0, 0));
-    // test_cloud.points.emplace_back(pcl::PointXYZ(0.0, 0.0, 10.0));
-    // test_cloud.points.emplace_back(pcl::PointXYZ(0.0, 0.0, 0.0));
-    // test_cloud.points.emplace_back(pcl::PointXYZ(0.0, 5.0, 0.0));
-
-    // test_cloud.points[0].intensity = 20.0;
-    // test_cloud.points[1].intensity = 40.0;
-    // test_cloud.points[2].intensity = 60.0;
-    // test_cloud.points[3].intensity = 80.0;
-    // test_cloud.points[4].intensity = 100.0;
-    // pcl_ros::transformPointCloud(test_cloud, test_2loud, test_transform);
-    // pubinit.publish(test_2loud);
-
-    /*pcl::ConvexHull<pcl::PointXYZ> cHull;
-    pcl::PointCloud<pcl::PointXYZ> cHull_points;
-    cHull.setInputCloud(test_cloud.makeShared());
-    cHull.reconstruct(cHull_points);
-    for (auto i = 0; i < cHull_points.size(); i++) {
-        ROS_INFO_STREAM("Point x: " << cHull_points.points[i].x << " y: " <<
-    cHull_points.points[i].y << " z: " << cHull_points.points[i].z);
-    }*/
-    /*        polygon poly;
-            append(poly, xypoint(1, 0));
-            append(poly, xypoint(-1, 0));
-            append(poly, xypoint(0, 1));
-            append(poly, xypoint(0, -1));
-            append(poly, xypoint(0, 0));
-            append(poly, xypoint(0, 0.1));
-            append(poly, xypoint(0, 0.2));
-            append(poly, xypoint(0.01, 1));
-            append(poly, xypoint(-0.01, 1));
-
-            polygon hull;
-            convex_hull(poly, hull);
-            ROS_INFO_STREAM("Hull: " << dsv(hull));
-
-            xypoint p;
-            centroid(hull, p);
-            ROS_INFO_STREAM("Centroid: " << dsv(p));
-    */
 
   } catch (tf::TransformException &ex) {
     ROS_WARN("%s", ex.what());
   }
   pubtest.publish(*land_cloud);
   double tstop = ros::Time::now().toSec();
-  // ROS_INFO_STREAM("land point time: " << tstop - tnow << " init: " <<
-  // pc2Stamp2 - pc2Stamp.toSec());
 }
 
 int main(int argc, char **argv) {
@@ -305,8 +205,6 @@ int main(int argc, char **argv) {
   pubcentroids =
       nh.advertise<pcl::PointCloud<pcl::PointXYZ>>("centroid_cloud", 1);
   pListener = new tf::TransformListener(ros::Duration(3.0));
-  // ros::Subscriber sub = nh.subscribe("velodyne_points", 1,
-  // velodyne_points_cb);
   ros::Subscriber sub = nh.subscribe<pcl::PointCloud<pcl::PointXYZI>>(
       "velodyne_points", 10, velodyne_points_cb);
   ros::spin();
