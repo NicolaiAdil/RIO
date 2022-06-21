603181ecd0f31f5e69cfa8a47b55ae96b67d1e4f
Jakob Jernsletten <jakobj@stud.ntnu.no>
2022-06-21 13:59:15 +0200

    #3548: Add land filtering

--- radar_processing/src/RadarPointsToClusters.cpp
+++ radar_processing/src/RadarPointsToClusters.cpp
@@ -26,8 +26,8 @@ PclClustering::PclClustering(ros::NodeHandle nh_in) : nh{nh_in}, pub_seq{0} {
     ROS_INFO_STREAM("Input frame set to: " << input_frame_id);
   }
   
-  if (!nh.getParam("/sensors/radar/frames/cluster_output", output_frame_id)) {
-    output_frame_id = "ned";
+  if (!nh.getParam("/sensors/radar/frames/output", output_frame_id)) {
+    output_frame_id = "body";
     ROS_WARN_STREAM(
         "No cluster output frame specified, points input in: " << output_frame_id);
   } else {
@@ -39,8 +39,6 @@ PclClustering::PclClustering(ros::NodeHandle nh_in) : nh{nh_in}, pub_seq{0} {
                                      &PclClustering::pcl_cb, this);
   hulls_pub = nh.advertise<jsk_recognition_msgs::PolygonArray>(hulls_topic, 10);
   scan_pub = nh.advertise<custom_msgs::RadarScan>(scan_topic, 10);
-
-  nh.param<std::string>("/sensors/radar/frames/output", output_frame, "body");
 
   double tolerance;
   int min_cluster_size, max_cluster_size;
@@ -54,73 +52,91 @@ PclClustering::PclClustering(ros::NodeHandle nh_in) : nh{nh_in}, pub_seq{0} {
 }
 
 void PclClustering::pcl_cb(const PointCloud::ConstPtr &cloud) {
-  // For all points convert to NED
-  pListener->waitForTransform("fixed", "radar", pc2Stamp, ros::Duration(1.0));
-  pListener->lookupTransform("fixed", "radar", pc2Stamp, transform);
-  pListener->lookupTransform("ned", "radar", pc2Stamp, transform2);
-  pListener->lookupTransform("fixed", "ned", pc2Stamp, fixed_ned_transform);
-  // For all points check if isLand and omit
-  // Make new cloud with only sea points.
+  ros::Time pc2Stamp = ros::Time::now();
+  geometry_msgs::TransformStamped transformStamped;
+  tf::StampedTransform fixed_radar_tf;
+  tf::StampedTransform ned_radar_tf;
+  tf::StampedTransform fixed_ned_tf;
+  pcl::PointCloud<pcl::PointXYZ> ned_cloud;
+  pcl::PointCloud<plc::PointXYZ> filtered_cloud;
+  pcl::PointCloud<plc::PointXYZ> output_cloud;
 
-  for (auto point : cloud){
 
-    if landmap->isLand(point.x, point.y){
+  try{
 
-    }
-  }
-  KdTree::Ptr tree{new KdTree};
-  tree->setInputCloud(cloud);
-  euclidean_clustering.setSearchMethod(tree);
-  euclidean_clustering.setInputCloud(cloud);
+    pListener->waitForTransform("fixed", input_frame_id, pc2Stamp, ros::Duration(1.0));
+    pListener->lookupTransform("fixed", input_frame_id, pc2Stamp, fixed_input_tf);
+    pListener->lookupTransform("ned", input_frame_id, pc2Stamp, ned_input_tf);
+    pListener->lookupTransform(output_frame_id, "ned", pc2Stamp, output_ned_tf);
 
-  // Extract clusters from the cloud and save their indices. cluster_indices[i]
-  // contains an array of each point in the original cloud that correspond to
-  // the i'th cluster.
-  std::vector<pcl::PointIndices> cluster_indices;
-  euclidean_clustering.extract(cluster_indices);
-
-  std_msgs::Header common_header;
-  common_header.seq = pub_seq;
-  common_header.stamp = ros::Time::now();
-  common_header.frame_id = output_frame;
-
-  jsk_recognition_msgs::PolygonArray hulls;
-  custom_msgs::RadarScan scan;
-  for (auto cit = cluster_indices.begin(); cit != cluster_indices.end();
-       ++cit) {
-
-    PointCloud::Ptr cluster{new PointCloud};
-    for (const auto &idx :
-         cit->indices) { // Fetch the points from the original cloud that
-                         // correspond to the i'th cluster
-      cluster->push_back((*cloud)[idx]);
+    pcl_ros::transformPointCloud(cloud, ned_cloud, ned_radar_tf)
+    for (const auto& point: ned_cloud->points){
+      if landmap->isLand(point.x, point.y){
+        filtered_cloud->push_back(point);
+      }
     }
 
-    PointCloud hull;
-    cloud_to_hull(cluster, hull);
-    geometry_msgs::PolygonStamped single_polygon;
-    pcl_hull_to_polygon(hull, single_polygon);
-    single_polygon.header = common_header;
-    hulls.polygons.push_back(single_polygon);
+    // Transform the land-filtered cloud to our output frame.  
+    pcl_ros::transformPointCloud(filtered_ned_cloud, output_cloud, output_ned_tf)
 
-    custom_msgs::RadarCluster cl;
-    cl.header = common_header;
-    // cl.type = custom_msgs::RadarCluster::EXTENDED;
-    cl.type = custom_msgs::RadarCluster::POINT;
-    geometry_msgs::Point centroid;
-    cloud_to_centroid(cluster, centroid);
-    cl.centroid = centroid;
-    cl.n_points = 1;
-    // cl.hull = single_polygon.polygon;
-    scan.radar_scan.push_back(cl);
+    KdTree::Ptr tree{new KdTree};
+    tree->setInputCloud(filtered_cloud);
+    euclidean_clustering.setSearchMethod(tree);
+    euclidean_clustering.setInputCloud(output_cloud);
+
+    // Extract clusters from the filtered cloud in output frame and save their indices. 
+    // cluster_indices[i] contains an array of each point in the original cloud that 
+    // correspond to the i'th cluster.
+
+    std::vector<pcl::PointIndices> cluster_indices;
+    euclidean_clustering.extract(cluster_indices);
+
+    std_msgs::Header common_header;
+    common_header.seq = pub_seq;
+    common_header.stamp = ros::Time::now();
+    common_header.frame_id = output_frame_id;
+
+    jsk_recognition_msgs::PolygonArray hulls;
+    custom_msgs::RadarScan scan;
+    for (auto cit = cluster_indices.begin(); cit != cluster_indices.end();
+        ++cit) {
+
+      PointCloud::Ptr cluster{new PointCloud};
+      for (const auto &idx :
+          cit->indices) { // Fetch the points from the output cloud that
+                          // correspond to the i'th cluster
+        cluster->push_back((*output_cloud)[idx]);
+      }
+
+      PointCloud hull;
+      cloud_to_hull(cluster, hull);
+      geometry_msgs::PolygonStamped single_polygon;
+      pcl_hull_to_polygon(hull, single_polygon);
+      single_polygon.header = common_header;
+      hulls.polygons.push_back(single_polygon);
+
+      custom_msgs::RadarCluster cl;
+      cl.header = common_header;
+      // cl.type = custom_msgs::RadarCluster::EXTENDED;
+      cl.type = custom_msgs::RadarCluster::POINT;
+      geometry_msgs::Point centroid;
+      cloud_to_centroid(cluster, centroid);
+      cl.centroid = centroid;
+      cl.n_points = 1;
+      // cl.hull = single_polygon.polygon;
+      scan.radar_scan.push_back(cl);
+    }
+
+    hulls.header = common_header;
+    hulls_pub.publish(hulls);
+
+    scan_pub.publish(scan);
+
+    pub_seq++;
   }
-
-  hulls.header = common_header;
-  hulls_pub.publish(hulls);
-
-  scan_pub.publish(scan);
-
-  pub_seq++;
+  catch (tf::TransformException &ex){
+    ROS_WARN("%s", ex.what())
+  }
 }
 
 void PclClustering::cloud_to_hull(const PointCloud::Ptr input_cloud,
@@ -178,23 +194,6 @@ int main(int argc, char **argv) {
 
   pListener = new tf::TransformListener(ros::Duration(3.0));
   landmap->initialize(refLat, refLon);
-  // The following is only used to publish the land_cloud once made.
-  // Should already be published when lidar_clustering is running.
-  // Consider removing.
-  land_cloud = new pcl::PointCloud<pcl::PointXYZ>();
-  land_cloud->header.frame_id = "fixed";
-  land_cloud->height = 1;
-  for (int n_int = -1000; n_int < 1000; n_int++) {
-    for (int e_int = -1000; e_int < 1000; e_int++) {
-      double n = double(n_int) * 0.5;
-      double e = double(e_int) * 0.5;
-      if (landmap->isLand(n, e)) {
-        land_cloud->points.push_back(pcl::PointXYZ(e, n, 0.0));
-        land_cloud->width += 1;
-      }
-    }
-  }
-  
   ros::spin();
   return 0;
 }
